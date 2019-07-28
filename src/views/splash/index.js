c522b13e504537eaf315138473ca81060e2dc84e
muhamadnabila <mbilalghz.gmail.com>
2019-07-28 22:34:29 +0700

    develop detail answer

--- src/views/splash/index.js
+++ src/views/splash/index.js
@@ -1,36 +1,36 @@
 import React, { useEffect } from 'react'
-import { 
-  View, 
+import {
+  View,
   Text,
-  ActivityIndicator 
+  ActivityIndicator
 } from 'react-native'
 import style from "./style"
 import * as firebase from 'firebase'
 
 
 const index = (props) => {
-  checkLoggedIn =() => {
-    firebase.auth().onAuthStateChanged( user => {
+  checkLoggedIn = () => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        props.navigation.navigate('login')
-      } else {
         props.navigation.navigate('dashboard')
+      } else {
+        props.navigation.navigate('login')
       }
     })
   }
 
   useEffect(() => {
+    checkLoggedIn()
     setTimeout(() => {
-      // checkLoggedIn()
       props.navigation.navigate('login')
     }, 2000)
   }, [])
-  
+
 
   return (
     <View style={style.container}>
       <Text style={style.title}>pindai✔️</Text>
-      <ActivityIndicator size="large"/>
+      <ActivityIndicator size="large" />
     </View>
   )
 }
