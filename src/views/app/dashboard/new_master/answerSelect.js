df7b14e8fa21e262945a989517112bc124bc7b9b
Dion <dionagoes@gmail.com>
2019-07-29 21:17:37 +0700

    edit something

--- src/views/app/dashboard/new_master/answerSelect.js
+++ src/views/app/dashboard/new_master/answerSelect.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { View, Text } from 'react-native'
 import RadioGroup from 'react-native-radio-buttons-group';
 
-const answerSelect = () => {
+const answerSelect = ({index, handleChange}) => {
     let [data, setData] = useState(
         [
             {
@@ -22,9 +22,20 @@ const answerSelect = () => {
             },
         ]
     )
+    console.log(index);
+    console.log(data.selected);
+
+    const onSelected = (data) => {
+        console.log(data)
+        let selected = data.find(datum => datum.selected === true)
+        console.log(`num: ${index+1}, selected: ${selected.value}`);
+        handleChange(selected.value, index)
+    }
+
     return (
-        <View>
-            <RadioGroup radioButtons={data} onPress={() => console.log('pressed')} flexDirection='row'/>
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent:'space-between', padding: 10, borderBottomColor: 'rgba(0,0,0,0.2)', borderBottomWidth: 1}}>
+            <Text style={{fontFamily: 'montserrat-black', fontSize: 20}}>{index + 1}</Text>
+            <RadioGroup radioButtons={data} onPress={onSelected} flexDirection='row'/>
         </View>
     )
 }
--- src/views/app/dashboard/new_master/index.js
+++ src/views/app/dashboard/new_master/index.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, FlatList, Picker } from "react-native";
+import { View, Text, TextInput, FlatList, Button, Dimensions } from "react-native";
 import Select from "./answerSelect"
+import { ScrollView } from "react-native-gesture-handler";
+import {LinearGradient} from 'expo-linear-gradient'
+import {connect} from 'react-redux'
+import {createSetSoal} from '../../../../../store/action'
 
-const NewMaster = () => {
+let {width, height} = Dimensions.get('window')
+
+const NewMaster = ({createSetSoal, user, navigation}) => {
   const [totalKey, setTotal] = useState("1")
   const [array, setArray] = useState(["A"])
   const [selected, setSelected] = useState("A")
+  const [title, setTitle] = useState('Untitled')
   // let arr=[]
+  console.log(title)
   function handleInput(num){
     console.log('input ke handle');
     
@@ -16,7 +24,6 @@ const NewMaster = () => {
     for(let i = 0 ; i < num ; i++){
       arr.push("A")
       console.log('ke trigger');
-      
     }
     setArray(arr)
     console.log(arr,'===');
@@ -24,50 +31,83 @@ const NewMaster = () => {
   }
 
   async function handleChange(value, index){
-    console.log("checccccccccccccck",index);
     let arraynew = array
     arraynew[index] = value
     await setArray(arraynew)
-    await setSelected
-    console.log("checccccccccccccck",array);
+  }
+
+  function save(){
+    let newobj = {}
+
+    array.forEach((arr, index) => {
+      // newObj = Object.assign(newObj, {[index+1]: })
+      newobj[parseInt(index+1)] = arr
+    })
+    console.log(newobj);
+    createSetSoal({
+      UserId: user.UserId,
+      title,
+      answerKey: newobj,
+      answers: []
+    })
+    navigation.navigate('Dashboard')
   }
 
   return (
-    <View style={{marginTop : 150, justifyContent : "center", alignItems : "center"}}>
-      {/* <Text>new master</Text> */}
-      <Text>Form Create Soal</Text>
-      <TextInput
-        style={{height : 40, borderColor : "gray", borderWidth : 1, width : 300, marginTop : 10, padding : 10}}
-        placeholder="title.."
-      />
-      <TextInput
-        style={{height : 40, borderColor : "gray", borderWidth : 1, width : 300, marginTop : 10, padding : 10}}
-        placeholder="folder name.."
-      />
-      <Text style={{marginTop : 15}}>Number of Key Answer</Text>
-      <TextInput
-        onChangeText={text => handleInput(text)}
-        value={totalKey}
-        keyboardType="numeric"
-        style={{height : 40, borderColor : "gray", borderWidth : 1, width : 70, padding : 10}}
-        placeholder="..."
-      />
-      <View>
-        <FlatList
-        horizontal={false}
-        data={array}
-        keyExtractor={(item,index) => index.toString()}
-        renderItem={({item,index})=>(
-          <Select/>
-        )}
-        />
 
-        {/* {array.map((item,index)=> <Text key={index}>{item}</Text>)} */}
+    <LinearGradient colors={['#2C5364', '#203A43', '#0F2027']}>
+      <View style={{height: height-60, padding: 10}}>
+        <View style={{height: height/3, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{color: 'white', fontSize: 20, fontFamily: 'montserrat-black', marginBottom: 10}}>New answer key</Text>
+          <View style={{width: '100%', marginBottom: 10}}>
+            <Text style={{color: 'white', fontFamily: 'montserrat-regular', marginBottom: 10, textAlign: 'center'}}>Subject Name: </Text>  
+            <TextInput
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              keyboardType="numeric"
+              style={{borderColor : "white", borderWidth : 2, padding : 5, borderRadius: 10, color: 'white', textAlign: 'center'}}
+              placeholder="Number of questions"
+            />
+          </View>
+          <View style={{width: '100%', marginBottom: 10}}>
+            <Text style={{color: 'white', fontFamily: 'montserrat-regular', marginBottom: 10, textAlign: 'center'}}># of Questions: </Text>  
+            <TextInput
+              onChangeText={text => handleInput(text)}
+              value={totalKey}
+              keyboardType="numeric"
+              style={{borderColor : "white", borderWidth : 2, padding : 5, borderRadius: 10, color: 'white', textAlign: 'center'}}
+              placeholder="Number of questions"
+            />
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+            <Button title="save" onPress={save}/>
+          </View>
+        </View>
+        <View style={{backgroundColor: 'white', borderRadius: 10, padding: 20, height: height - 20 - (height/3)}}>
+          <FlatList
+            data={array}
+            style={{marginBottom: 60}}
+            keyExtractor={(item,index) => index.toString()}
+            renderItem={({item,index})=>(
+              <View>
+                <Select index={index} handleChange={handleChange}/>
+              </View>
+            )}
+          />
+        </View>
       </View>
-
-
-    </View>
+      </LinearGradient>
   );
 };
 
-export default NewMaster;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = {
+  createSetSoal
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewMaster);
