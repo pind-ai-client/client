ca90b25529932f4cd9227f6c25f457c01299a67d
rizky <andijani21.rizky@gmail.com>
2019-07-29 13:31:30 +0700

    add navigation setting

--- src/navigation.js
+++ src/navigation.js
@@ -21,7 +21,7 @@ import Dash from "./views/app/dashboard";
 import DetailMaster from "./views/app/dashboard/detail_master";
 import DetailAnswer from "./views/app/dashboard/detail_master/detail_answer";
 import NewMaster from "./views/app/dashboard/new_master";
-import Settings from "./views/app/dashboard/settings";
+import SettingsMaster from "./views/app/dashboard/settings";
 import Login from "./views/login";
 import Splash from "./views/splash"
 import Camera from "./views/app/dashboard/detail_master/Camera";
@@ -55,6 +55,13 @@ let Dashboard = createStackNavigator({
   no_appbar
 );
 
+let Settings = createStackNavigator({
+  setting: SettingsMaster,
+  acc: Account,
+  help : Help
+},
+no_appbar
+);
 
 let BottomNav = createBottomTabNavigator({
   Dashboard,
