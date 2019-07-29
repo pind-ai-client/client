b09e27b0740f085d03f45ae1abfb3228dbf957f5
robbycp <robbycaesar@gmail.com>
2019-07-29 18:18:29 +0700

    login auth on state done

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { View, Text, TouchableHighlight, Button } from "react-native";
 import style from "./style";
 import { Google } from 'expo'
+import * as GoogleSignIn from 'expo-google-sign-in';
 import * as Facebook from 'expo-facebook'
 import firebase from '../../api/firebase'
 
@@ -10,6 +11,7 @@ import { login } from '../../../store/action'
 
 const Login = ({ navigation, login }) => {
   useEffect(() => {
+    // initAsync()
     firebase.auth().onAuthStateChanged((user) => {
       if (user != null) {
         console.log("We are authenticated now!");
@@ -18,16 +20,42 @@ const Login = ({ navigation, login }) => {
     });
   }, []);
 
-   loginWithGoogle = async () => {
+  // first attempt
+  // loginWithGoogle = async () => {
+  //   try {
+  //     const result = await Google.logInAsync({
+  //       androidClientId:
+  //         "304290495073-17lh08lqersl4rpqlq9rqdc0hqos47ap.apps.googleusercontent.com",
+  //       scopes: ["profile", "email"]
+  //     })
+  //     if (result.type === "success") {
+  //       console.log('login google')
+  //       console.log(result.user)
+  //       login({
+  //         userName: result.user.name,
+  //         email: result.user.email,
+  //         UserId: result.user.id,
+  //         photoUrl: result .user.photoUrl
+  //       })
+  //       navigation.navigate('dashboard')
+  //     } else {
+  //       console.log("cancelled")
+  //     }
+  //   } catch (e) {
+  //     console.log("error", e)
+  //   }
+  // }
+
+  // first attempt
+  loginWithGoogle = async () => {
     try {
       const result = await Google.logInAsync({
         androidClientId:
-          "304290495073-17lh08lqersl4rpqlq9rqdc0hqos47ap.apps.googleusercontent.com",
+          "304290495073-kltkc2u9c27gmvan6u2u60rpavh76eq1.apps.googleusercontent.com",
         scopes: ["profile", "email"]
       })
       if (result.type === "success") {
-        console.log('login google')
-        // console.log(result.user)
+        const credential = await firebase.auth.GoogleAuthProvider.credential(result.idToken, result.accessToken)
         login({
           userName: result.user.name,
           email: result.user.email,
@@ -35,6 +63,12 @@ const Login = ({ navigation, login }) => {
           photoUrl: result .user.photoUrl
         }, navigation.navigate)
         // navigation.navigate('dashboard')
+        // })
+        // navigation.navigate('dashboard')
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          // Handle Errors here.
+          console.log(error)
+        });
       } else {
         console.log("cancelled")
       }
@@ -66,6 +100,8 @@ const Login = ({ navigation, login }) => {
     <View style={style.container}>
       <Button title="Sign in with Facebook" onPress={() => loginWithFacebook() } />
       <Button title="Sign in with Google" onPress={() => loginWithGoogle()} />
+      {/* <Button title="Sign in with Google" onPress={() => signInWithGoogleAsync()} /> */}
+      {/* <Button title="Sign in with Google" onPress={() => signInAsync()} /> */}
     </View>
   );
   
